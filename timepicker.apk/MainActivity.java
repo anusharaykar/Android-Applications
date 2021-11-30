b0afe8a0167277ecc0f378d77bcb4e479a725230
Anusha manjunath raykar <80142716+anusharaykar@users.noreply.github.com>
2021-11-30 22:29:30 +0530

    Create MainActivity.java

---
+++ timepicker.apk/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.cal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        EditText editText=findViewById(R.id.editTextTextPersonName);
+        editText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar calendar=Calendar.getInstance();
+                int hour=calendar.get(Calendar.HOUR_OF_DAY);
+                int min=calendar.get(Calendar.MINUTE);
+                TimePickerDialog timePickerDialog=new TimePickerDialog(MainActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        editText.setText(hourOfDay+":"+minute);
+                    }
+                },hour,min,true);
+                timePickerDialog.setTitle("pick a time!!");
+                timePickerDialog.show();
+            }
+        });
+    }
+}
